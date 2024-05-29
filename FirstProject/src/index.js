1e397c400589fa7dd5bb683b2692448a36983d14
Vijay Anand <139949005+vijayanand1505@users.noreply.github.com>
2024-05-29 08:53:00 +0530

    Update index.js

--- FirstProject/src/index.js
+++ FirstProject/src/index.js
@@ -8,7 +8,7 @@ import Footer from "./Footer";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> 
+    <App />
     <Footer />
   </React.StrictMode>
 );
